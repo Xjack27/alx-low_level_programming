3d9f0258e6912dbb578fea45df80035ea1f18bc8
Aidan <dark_shadow271@hotmail.com>
2022-06-06 18:44:05 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-int main(void) {
-	char Prog[55];
-	strcpy(Prog, '"Programming is like building a multilingual puzzle');
-	puts(Prog);
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return(0)
 }
