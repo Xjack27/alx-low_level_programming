30b68a449dab6d0ea0a280544c767ca40761d842
Aidan <dark_shadow271@hotmail.com>
2022-06-06 17:42:05 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,1 @@
-#!/bin/bash
-#include <stdio.h>
-int main()
-{
-char Prog[] = "Programming is like building a multilingual puzzle";
-puts(Prog);
-return(0);
-}
+puts(Programming is like building a multilingual puzzle)
