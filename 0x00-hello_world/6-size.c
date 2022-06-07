67d4c3c7fdb928979ecc975f5964471db6abc0c4
Aidan <dark_shadow271@hotmail.com>
2022-06-06 20:45:38 -0700

    7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-
+int main(void) {
 	int printf("Size of a char: %zu\n byte(s)", sizeof(char));
 
 	int printf("Size of an int: %zu\n byte(s)", sizeof(int));
@@ -12,4 +12,4 @@
 	int printf("Size of a float: %zu\n byte(s)", sizeof(float));
 
 	return (0);
-
+}
