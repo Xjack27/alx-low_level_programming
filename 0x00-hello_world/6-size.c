0e76a20746cc5a66cdef6cc4c4cc1a82e3e1d243
Aidan <dark_shadow271@hotmail.com>
2022-06-06 20:33:29 -0700

    7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,14 @@
 int main(void)
 {
 printf ("Size of a char: %zu\n byte(s)", sizeof(char));
+
 printf ("Size of an int: %zu\n byte(s)", sizeof(int));
+
 printf ("Size of a long int: %zu\n bytes(s)", sizeof(long));
+
 printf ("Size of a lon long int: %zu\n byte(s)", sizeof(long long));
+
 printf ("Size of a float: %zu\n byte(s)", sizeof(float));
+
 return (0);
 }
