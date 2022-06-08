0c63f16c3e0b0fe9b38977fc145c3558cb27dea8
Aidan <dark_shadow271@hotmail.com>
2022-06-08 02:52:36 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int write(int filedes, const char *buf, unsigned int nbyte);
-int main( int argc, char** argv )
-{
-write(1,"_putchar",9);
-return (0)
+int main( int argc, char** argv ) {
+
+	write(1,"_putchar",9);
+
+	return (0)
 }
